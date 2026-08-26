0adc438576db3a1c44ca809553e7777a5fbc83d4
IIIIIIlllllIIIIII <64425059+pale9876@users.noreply.github.com>
2026-08-27 01:41:42 +0900

    update

--- script/resource/blade_module_stat.gd
+++ script/resource/blade_module_stat.gd
@@ -6,11 +6,13 @@ class_name BladeModuleStat
 
 
 @export_range(0., 1., .01) var damage_ratio: float = 1.
+
 @export_enum(
 	"Knockback",
 	"Aerial",
-	"DownHit",
-) var after_state: String
+	"Pound",
+) var aftermath: String = "Knockback"
+
 @export var stun_frame: int = 10
 @export var force: Vector2
 @export var disturb_motion: bool = true # == 피격자의 모션 속도에 영향을 줍니다.
---
+++ script/resource/chieftain_information.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+
+@export var name: StringName
+@export var age: int = 15
+@export var max_count: int = 3 # 통제할 수 있는 인원
+@export var sprite_module_scene: PackedScene
